cf03cb90522b7bc1894c5dd4e5e250fb251618f2
pajid9763 <pajid9763@wrdsb.ca>
2020-10-08 13:58:12 +0000

    finished program that finds largest number

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * program to find largest number in an array
+ * @author Darian
  */
 public class Main {
 
@@ -9,7 +10,24 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+
+    int numbers[] = new int[10];
+
+    // ask the user to enter 10 integers
+    System.out.println("Please enter in 10 integers to put into the array");
+    for (int i = 0; i < numbers.length; i++){
+      numbers[i] = input.nextInt();
+    }
+
+    int highest = numbers[0];
+
+    for (int i = 0; i < numbers.length; i++){
+      if (highest < numbers[i])
+        highest = numbers[i];
+      }
+      System.out.println("The largest number is " + highest);
+    }
     
   }
-}
+
